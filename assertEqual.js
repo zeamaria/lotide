fb39e8002dab3ce465d7753c8eddf646ec21fd3e
zeamaria <lingardzea@gmail.com>
2021-03-30 04:21:23 +0000

    1st commit

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        return '✅✅✅ Assertion Passed: ' + actual + ' === ' + expected;
+    } else {
+        return '🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected;
+    }
+};
+
+// TEST CODE
+
+console.log(assertEqual(1, 1));
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
