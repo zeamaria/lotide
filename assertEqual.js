2a7a592f6240d1448ad8df6a52f6e9a9e364fda1
zeamaria <lingardzea@gmail.com>
2021-03-30 04:33:44 +0000

    update fancier strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
     if (actual === expected) {
-        return '✅✅✅ Assertion Passed: ' + actual + ' === ' + expected;
+        return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
     } else {
-        return '🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected;
+        return `🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`;
     }
 };
 
