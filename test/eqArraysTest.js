b16a0d0a65f2229af2641dbebb8e1cbdc3572432
zeamaria <lingardzea@gmail.com>
2021-04-06 06:17:37 +0000

    refactoring

---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// TEST 
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
